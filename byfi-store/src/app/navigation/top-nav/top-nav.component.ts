ab0d766285f9be465e6bf257e671ea521b1c4399
Sankepally-Yashwanth-Reddy <t_yashwanthreddy.sankepally@splashbi.com>
2024-10-15 09:16:04 +0530

    modified header

--- byfi-store/src/app/navigation/top-nav/top-nav.component.ts
+++ byfi-store/src/app/navigation/top-nav/top-nav.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-nav',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule],
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.scss'
 })
 export class TopNavComponent {
+
+  public routesArray = [
+  ];
+
+  constructor() { }
+
 
 }
