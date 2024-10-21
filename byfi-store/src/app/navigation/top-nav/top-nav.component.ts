add21be675518998d7f136558d74648243a34c34
Sankepally-Yashwanth-Reddy <t_yashwanthreddy.sankepally@splashbi.com>
2024-10-21 09:15:57 +0530

    commit

--- byfi-store/src/app/navigation/top-nav/top-nav.component.ts
+++ byfi-store/src/app/navigation/top-nav/top-nav.component.ts
@@ -1,62 +1,17 @@
 import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
   standalone: true,
-  imports: [NgIf, CommonModule, RouterModule],
+  imports: [NgIf, CommonModule],
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.scss'
 })
 export class TopNavComponent {
 
   public routesArray = [
-    {
-      path: 'home',
-      name: 'Home'
-    },
-    {
-      path: 'mobiles',
-      name: 'Mobiles'
-    },
-    {
-      path: 'electronics',
-      subRoutes: [
-        {
-          path: 'laptops',
-          name: 'Laptops'
-        },
-        {
-          path: 'e-tablets',
-          name: 'Tablets/Pads'
-        },
-        {
-          path: 'electronics-accessories',
-          name: 'Electronic Accessories'
-        }
-      ]
-    },
-    {
-      path: 'fashion',
-      subRoutes: [
-        {
-          path: 'fashion/men',
-          name: 'Men\'s Fashion'
-        },
-        {
-          path: 'fashion/women',
-          name: 'Women\'s Fashion'
-        },
-        {
-          path: 'fasion/kids',
-          name: 'Kids'
-        }
-      ]
-    }
   ];
-
-
 
   constructor() { }
 
